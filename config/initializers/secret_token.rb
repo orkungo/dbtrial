a76641d8df08bf7fff88f2162b501d2c23785d4b
orkun <orkun@orkun-virtual-machine.(none)>
2012-09-17 11:29:45 +0200

    first commit with DB

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dbtrial::Application.config.secret_token = 'be5c015cbca391ff0a378e2bdf20750b50ee0dacf7549b3c587e7e19eadd5a021fad9a3e31af361077cc6f943d63a73d981a5cb3788b33cc4538907ea6d28f74'
